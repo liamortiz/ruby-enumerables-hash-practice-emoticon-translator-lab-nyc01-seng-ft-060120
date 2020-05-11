3743192aabefe510a3c60fb5a3b871e3c8c9e27a
Leamsi Escribano <leamsii.e@gmail.com>
2020-05-11 23:23:19 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,16 @@
 # require modules here
+require "yaml"
 
-def load_library
-  # code goes here
+def load_library(path)
+  library = YAML.load_file(path)
+  e = {:english => {}, :japanese => {}}
+  
+  library.each_pair do |key, value|
+    e[:english][key.to_sym] = value[0]
+    e[:japanese][key.to_sym] = value[1]
+  end
+  pp e
+  return e
 end
 
 def get_japanese_emoticon
